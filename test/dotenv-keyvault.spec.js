f2508f88972e940ff32a621abb108e691695608e
Will Morgan <will@willmorgan.co.uk>
2017-11-13 15:29:18 +0000

    Fix stub returning other function

--- test/dotenv-keyvault.spec.js
+++ test/dotenv-keyvault.spec.js
@@ -16,16 +16,18 @@ describe('dotenv-keyvault', () => {
     });
     it('takes an Azure Active Directory token as a function', (done) => {
         const fakeADToken = 'SOME_TOKEN';
-        const configSpy = sinon.stub().returns(() => fakeADToken);
+        const configSpy = sinon.stub().returns(fakeADToken);
         dotenvKeyvault.config({ adToken: configSpy }).then(() => {
             expect(configSpy.called).to.equal(true);
             done();
         }).catch(done);
     });
-    it('calls keyvault with the AD access token', () => {
-        dotenvKeyvault.config({ adToken: 'SOME_TOKEN' });
-        expect(requestSpy.called).to.equal(true);
-        expect(requestSpy.args[0][0]).to.have.nested.property('headers.Authorization').that.contains('SOME_TOKEN');
+    it('calls keyvault with the AD access token', (done) => {
+        dotenvKeyvault.config({ adToken: 'SOME_TOKEN' }).then(() => {
+            expect(requestSpy.called).to.equal(true);
+            expect(requestSpy.args[0][0]).to.have.nested.property('headers.Authorization').that.contains('SOME_TOKEN');
+            done();
+        }).catch(done);
     });
     it('does nothing with non-secret variables', () => {
         dotenvKeyvault.config({ adToken: () => 'SOME_TOKEN' });
